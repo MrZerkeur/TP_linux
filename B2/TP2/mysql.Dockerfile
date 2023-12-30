6645d7fbd7d40c4f239bf8520c8c7563bc390aa6
axel <axel.broquaire@outlook.fr>
2023-12-30 14:25:26 +0100

    début tp2

---
+++ B2/TP2/mysql.Dockerfile
@@ -0,0 +1,2 @@
+FROM mysql
+ONBUILD RUN echo "App is ready on http://localhost:80 BUT the DB can take some time to be up !"
---
+++ B2/TP2/sql/seed.sql
@@ -0,0 +1,9 @@
+CREATE TABLE random (
+    id int,
+    name VARCHAR(255)
+);
+
+INSERT INTO random(id, name) VALUES
+(1,'RANDOM'),
+(2,'BULLSHIT'),
+(3,'GO');
